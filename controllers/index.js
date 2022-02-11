f76f969f97fb5df3385e28318e3b353198fdf122
playground <connie.s.barrantes@gmail.com>
2022-02-10 23:31:15 -0500

    created home-routes.js, public folder, character.js and character handlebars.

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 //const apiRoutes = require('./api');
 // -- Adding handlebars routing
-// const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-route.js');
 
 // router.use('/api', apiRoutes);
 
 // // -- Giving handlebar routing params to Router
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 
 //-- if gets here when rounting, throw 404
 router.use((req, res) => {
