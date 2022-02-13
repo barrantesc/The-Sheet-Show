ef36820d0400858c6e16589bee6d5a37b0da5fc7
Erik Plachta <eplachta@gmail.com>
2022-02-13 11:23:25 -0500

    Add adjustment to seeds/connection_seqeuelize.js to include JawsDB Env Variable.

--- seeds/connection_sequelize.js
+++ seeds/connection_sequelize.js
@@ -2,18 +2,28 @@
 const Sequelize = require('sequelize'); // for drop add tables in employee_db
 require('dotenv').config(); //-- for local variable caching
 
-//-- MAKE sequelize obj to play as ORM
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.SERVER_PATH,
-    dialect: 'mysql',
-    port: 3306
-  },
-  console.log(`//-- Connection MYSQL database with npm package Sequelize success!`)
-);
 
+//-- Create placeholder for sequelize
+let sequelize;
+
+
+//-- MAKE sequelize obj for JawsDB on Heroku instance
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} 
+//-- MAKE sequelize obj for local MySQL instance
+else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.SERVER_PATH,
+      dialect: 'mysql',
+      port: 3306
+    },
+    console.log(`//-- Connection MYSQL database with npm package Sequelize success!`)
+  );
+}
 //-- exporting created sequelize obj
 module.exports = sequelize;
