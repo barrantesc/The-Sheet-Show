14c1f942706f9ca4863cf04ff75f7561587f8525
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-02-15 00:56:49 -0500

    function for calculating proficiency bonus based on player level

--- public/javascript/character-creator.js
+++ public/javascript/character-creator.js
@@ -65,8 +65,51 @@ const retrieveClassInfo = async function (className) {
     }
 }
 
+<<<<<<< Updated upstream
 
 displayRacesOptions();
 displayClassesOptions();
 retrieveRaceInfo(raceName);
 retrieveClassInfo(className);
+=======
+// Calculate ability scores
+    // Yields number between 6 - 18, to simulate rolling 4d6, re-rolling any 1s, and dropping lowest number
+const abilityScoresCalc = function () {
+    const score = Math.floor((Math.random() * 12) + 6);
+    console.log(score);
+    return score;
+};
+
+// Calculate modifiers
+    // takes ability score, subtracts 10, divides by 2, rounds up to nearest integer
+
+    // temporary variable to test modifier calculator
+let score = 15;
+
+const modifierCalc = function (score) {
+    const modifier = Math.ceil((score - 10) / 2);
+    console.log(modifier);
+    return modifier;
+};
+
+// Calculate Proficiency Bonus
+    // determined by player level (starting from level 1, the bonus increases by 1every four levels)
+
+    // temporary variable to test proficiency bonus calculator
+let playerLevel = 13;
+
+const profBonusCalc = function (playerLevel) {
+    let profBonus = Math.ceil(playerLevel/4)+1;
+    console.log(profBonus);
+    return profBonus;
+};
+
+
+// displayRacesOptions();
+// displayClassesOptions();
+// retrieveRaceInfo(raceName);
+// retrieveClassInfo(className);
+// abilityScoresCalc();
+// modifierCalc(score);
+profBonusCalc(playerLevel);
+>>>>>>> Stashed changes
