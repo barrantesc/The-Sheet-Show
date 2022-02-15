bc09282862d268bcfc27fec424b512b9f077a4b8
Mary Margaret <mmlawton15@gmail.com>
2022-02-15 10:05:27 -0500

    trying to figure out what the login bug is doing but haven't gotten anywhere, will tackle this with the team tonight. focusing on css styling now

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,26 +1,5 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        alert('Login Success.');
-        document.location.replace('/');
-      } else {
-        alert('Failed to log in.');
-      }
-    }
-  };
-  
-  const signupFormHandler = async (event) => {
+//SIGN UP FORM HANDLER
+const signupFormHandler = async (event) => {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
@@ -40,7 +19,37 @@ const loginFormHandler = async (event) => {
         alert('Failed to sign up.');
       }
     }
-  };
+};
+
+
+
+
+//LOGIN FORM HANDLER
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: {'Content-Type': 'application/json'}
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+};
+  
+
   
   document
     .querySelector('.login-form')
