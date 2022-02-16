3fd347a095ee231c8e0dd9650b6e85bd5fd9829b
Christiana Sullivan Morales <chrismoralesmiranda@gmail.com>
2022-02-16 16:32:48 -0500

    add functionality to retrieve user input from form and turn into object to save to database

--- public/javascript/character-creator.js
+++ public/javascript/character-creator.js
@@ -1,41 +1,37 @@
 // Retrieve selected race info
-
-    // temporary variable to test fetch race info 
-let raceName = 'human';
-
 const retrieveRaceInfo = async function (raceName) {
     const apiUrl = `https://www.dnd5eapi.co/api/races/${raceName}`;
 
     const response = await fetch(apiUrl);
     if (response.ok) {
         const raceData = await response.json();
-        // saving data to variables
-        const abilityBonuses = raceData.ability_bonuses;
-        const alignment = raceData.alignment;
-        const sizeDesc = raceData.size_description;
-        const languages = raceData.languages;
+        // saving data to object
+        const raceInfo = {
+            ability_bonuses : raceData.ability_bonuses,
+            alignment : raceData.alignment,
+            languages : raceData.languages
+        }
 
-        console.log(abilityBonuses, alignment, sizeDesc, languages);
+        return raceInfo;
     } else {
         console.log(err);
     }
 };
 
 // Retrieve selected class info
-
-    // temporary variable to test fetch class info
-let className = 'sorcerer';
-
 const retrieveClassInfo = async function (className) {
     const apiUrl = `https://www.dnd5eapi.co/api/classes/${className}`;
 
     const response = await fetch(apiUrl);
     if (response.ok) {
         const classData = await response.json();
-        // saving to variables
-        const profChoicesAllowed = classData.proficiency_choices[0].choose;
-        const profArray = classData.proficiency_choices[0].from;
-        console.log(profChoicesAllowed, profArray);
+        // saving to object
+        const classInfo = {
+            choice_number : classData.proficiency_choices[0].choose,
+            prof_array : classData.proficiency_choices[0].from
+        }
+
+        return classInfo;
     }
 }
 
@@ -61,19 +57,90 @@ const modifierCalc = function (score) {
 
 // Calculate Proficiency Bonus
     // determined by player level (starting from level 1, the bonus increases by 1every four levels)
-
-    // temporary variable to test proficiency bonus calculator
-let playerLevel = 13;
-
 const profBonusCalc = function (playerLevel) {
     let profBonus = Math.ceil(playerLevel/4)+1;
-    console.log(profBonus);
     return profBonus;
 };
 
-// retrieveRaceInfo(raceName);
-// retrieveClassInfo(className);
+// turn an array of languages into a string for storage in model
+    // sequelize will only allow storage as array if using PostgresSQL)
+    // takes array of objects, retrieves name property from each and pushes it to a new array
+    // joins array items into a string
+const stringFromArray = function (array) {
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+        arr.push(array[i].name);
+    }
+    return arr.join(" ");
+}
+
+// choose x amount of random element from array - for choosing allowed # of proficiencies, and turning to string
+    // takes array of objects, retrieves index property from each and pushes it to a new array
+    // selects a random element from array x number of times, pushes each to a different new array
+    // joins the randomly selected elements into a string
+    // -- still need to figure out how to remove selected element from arr after pushing to newArr to avoid duplicates, but code breaks with obvious the splice() solution
+const randomFromArray = function (array, n) {
+    let arr = [];
+    let newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        arr.push(array[i].index);
+    }
+    for (let i = 0; i < n; i++) {
+        let idx = Math.floor(Math.random() * (array.length));
+        newArr.push(arr[idx]);
+        // arr.splice(idx);
+    }
+    // console.log(arr);
+    // console.log(newArr);
+    return newArr.join(" ");
+}
+
 // abilityScoresCalc();
 // modifierCalc(score);
-profBonusCalc(playerLevel);
+
+// take user input from character creator form to turn into object to generate a character and save to database
+async function newCharFormHandler(event) {
+    event.preventDefault();
+
+    // info retrieved directly from form, or from calculations from form input values
+    const name = document.querySelector('#char-name').value;
+    const race = document.querySelector('#char-race').value;
+    const char_class = document.querySelector('#char-class').value;
+    const gender = document.querySelector('#char-gender').value;
+    const age = document.querySelector('#char-age').valueAsNumber;
+    const player_level = document.querySelector('#char-level').valueAsNumber;
+    const proficiency_bonus = profBonusCalc(document.querySelector('#char-level').valueAsNumber);
+
+    // info retrieved from api/races based on user input
+    const raceInfo = await retrieveRaceInfo(document.querySelector('#char-race').value);
+    const alignment = raceInfo.alignment;
+    const langString = stringFromArray(raceInfo.languages);
+
+    // info retrieved from api/classes based on user input
+    const classInfo = await retrieveClassInfo(document.querySelector('#char-class').value);
+    const profString = randomFromArray(classInfo.prof_array, classInfo.choice_number);
+
+    // const title = document.querySelector('input[name="post-title"]').value;
+    // const post_url = document.querySelector('input[name="post-url"]').value;
+
+    // const response = await fetch('api/posts', {
+    //     method: 'POST', 
+    //     body: JSON.stringify({
+    //         title,
+    //         post_url
+    //     }),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // });
+
+    // if (response.ok) {
+    //     document.location.replace('/dashboard');
+    // } else {
+    //     alert(response.statusText);
+    // }
+};
+
+document.querySelector('#character-form').addEventListener('submit', newCharFormHandler);
+
 
