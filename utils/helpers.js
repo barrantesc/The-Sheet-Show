8a63ed10ab5c16ad65acc5ea4f091f8753c423e9
Erik Plachta <eplachta@gmail.com>
2022-02-20 13:02:35 -0500

    Add comments to helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,12 @@
-
+//-- Formatting Date
 const format_date = date => {
   return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
     date
   ).getFullYear()}`;
 }
 
+
+//-- EXPORTS
 module.exports = {
   format_date,   
 }
-  
