5880b62ab4214e03b555af9f348f30068b3c7c39
Erik Plachta <eplachta@gmail.com>
2022-02-20 15:43:27 -0500

    Add route back to / when closing modal

--- public/javascript/hero-card.js
+++ public/javascript/hero-card.js
@@ -33,11 +33,13 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
+    document.location.replace('/');
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.location.replace('/');
     }
 };
